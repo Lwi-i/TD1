75b523b8dea5368b8fdcfba250cb0a911fce5127
dovillaire louis <louis.dovillaire@etu.umontpellier.fr>
2024-09-12 09:40:42 +0200

    m

--- src/main/java/fr/iut/editeur/commande/CommandeClear.java
+++ src/main/java/fr/iut/editeur/commande/CommandeClear.java
@@ -2,24 +2,32 @@ package fr.iut.editeur.commande;
 
 import fr.iut.editeur.document.Document;
 
+/**
+ * Descriptionnnnnnnnn
+ */
 public class CommandeClear extends CommandeDocument{
 
 
-
-
-
-
-        public CommandeClear(Document document, String[] parameters) {
-            super(document, parameters);
-        }
-
-        @Override
-        public void executer() {
-
-
-            this.document.clear();
-            super.executer();
-        }
-
+    /**
+     *
+     * @param document Ceci est un jolie documment
+     * @param parameters Ceci est un parametreeeeeeeesss XDDD
+     */
+    public CommandeClear(Document document, String[] parameters) {
+        super(document, parameters);
     }
+
+
+    @Override
+    /**
+     * EXECUTION !!!!!
+     */
+    public void executer() {
+
+
+        this.document.clear();
+        super.executer();
+    }
+
+}
 
